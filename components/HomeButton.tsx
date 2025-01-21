6ebc3ded3b2769fe901c36e1078f99842e06cfe7
Vanessa Cruz <vaneycaroline@gmail.com>
2025-01-21 15:48:38 -0300

    feat: fix button size

--- components/HomeButton.tsx
+++ components/HomeButton.tsx
@@ -3,7 +3,7 @@ import { FaArrowRight } from 'react-icons/fa';
 export default function HomeButton() {
   return (
     <>
-      <button className="text-base rounded-md bg-black text-white lg:w-1/3 w-3/4 h-14 flex items-center gap-2 justify-center absolute lg:mt-0 mt-[326px]">
+      <button className="text-base rounded-md bg-black text-white lg:w-2/12 w-3/4 h-14 flex items-center gap-2 justify-center absolute lg:mt-0 mt-[326px]">
         Get started <FaArrowRight />
       </button>
     </>
